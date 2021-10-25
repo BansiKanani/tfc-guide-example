0a58f85129fa454015db25b7d25ac10e4a3aaca5
Bansi Kanani <bansikumar.r@acko.tech>
2021-10-25 18:34:16 +0530

    add output vars

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,11 @@ output "instance_arn" {
   value = aws_instance.ubuntu.arn
 }
 
+output "private_ip" {
+  value = aws_instance.ubuntu.private_ip
+}
+
+output "key_name" {
+  value = aws_instance.ubuntu.key_name
+}
+
